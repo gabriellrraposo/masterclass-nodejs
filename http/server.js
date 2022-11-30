38aea33772b3064215f8ca3870281973f7b983bc
gabriellrraposo <gabriellucasrosaraposo@gmail.com>
2022-11-30 08:54:59 -0300

    Define tipos de arquivos permitidos, para não travar a execução

--- http/server.js
+++ http/server.js
@@ -1,12 +1,17 @@
 const http = require('http')
 const fs = require('fs')
 const path = require('path')
+const { extname } = require('path')
 
 http.createServer((req, res) => {
     const file = req.url === '/' ? 'index.html' : req.url
     const filePath = path.join(__dirname, 'public', file)
+    const extname = path.extname(filePath)
 
     const allowedFileTypes = ['.html', '.js', '.css']
+    const allowed = allowedFileTypes.find(item => item == extname)
+
+    if (!allowed) return
     fs.readFile(
         filePath, 
         (err, content) => {
